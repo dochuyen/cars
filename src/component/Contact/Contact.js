2119ad54be687dd07e4d8336a296010ff636b88d
dankieu <kieudinhdan02@gmail.com>
2023-03-16 22:45:59 +0700

    con

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+import classNames from "classnames/bind";
+import styles from "./Contact.scss";
+const cx = classNames.bind(styles);
 const Contact = () => {
   return (
-    <div>
-    Contact
-    </div>
-  )
-}
+    <div className={cx("wrapper")}>
+      <h1>Contatti & Info</h1>
+      <Container>
+        <Row>
+          <Col xs={12} md={8}>
+            xs=12 md=8
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+          <Col xs={6} md={4}>
+            xs=6 md=4
+          </Col>
+        </Row>
 
-export default Contact
+        {/* Columns are always 50% wide, on mobile and desktop */}
+        <Row>
+          <Col xs={6}>xs=6</Col>
+          <Col xs={6}>xs=6</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
